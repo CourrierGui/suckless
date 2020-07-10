c6bbcff901451e2702759f31f522789f711fa1a7
Courrier Guillaume <courrier.guillaume.123@gmail.com>
2020-07-10 14:49:57 +0200

    remove comment

--- dwmblocks/config.h
+++ dwmblocks/config.h
@@ -1,4 +1,5 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
+
 static const Block blocks[] = {
 	/*Icon*/  /*Command*/                  /*Update Interval*/  /*Update Signal*/
 	{"",      "taskbar",                  30,                   9},
@@ -16,14 +17,8 @@ static const Block blocks[] = {
 	//{"",	"weather",	18000,	5},
 	//{"",	"mailbox",	180,	12},
 	/* {"",	"nettraf",	1,	16}, */
-	//{"",	"clock",	60,	1},
 	//{"",	"help-icon",	0,	15},
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
-static char delim = '\0';
-
-// Have dwmblocks automatically recompile and run when you edit this file in
-// vim with the following line in your vimrc/init.vim:
-
-// autocmd BufWritePost ~/.local/src/dwmblocks/config.h !cd ~/.local/src/dwmblocks/; sudo make install && { killall -q dwmblocks;setsid dwmblocks & }
+static char delim = '|';
