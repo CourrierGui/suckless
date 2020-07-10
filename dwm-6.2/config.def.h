bc45c347a7bdd78cc7e15534991a246c156898a6
Courrier Guillaume <courrier.guillaume.123@gmail.com>
2020-07-10 15:23:23 +0200

    reduce status bar font size

--- dwm-6.2/config.def.h
+++ dwm-6.2/config.def.h
@@ -14,10 +14,10 @@ static const int smartgaps          = 0;        /* 1 means no outer gap when the
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const char *fonts[]          = {
-	"Linux Libertine Mono:size=15",
-	"JoyPixels:pixelsize=15:antialias=true:autohint=true"
+	"Linux Libertine Mono:size=12",
+	"JoyPixels:pixelsize=12:antialias=true:autohint=true"
 };
-static const char dmenufont[]       = "Linux Libertine Mono:size=15";
+static const char dmenufont[]       = "Linux Libertine Mono:size=12";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
