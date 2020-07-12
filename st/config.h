8c84df32ce700d9d8ff667c64afb9102b0c74ad5
Courrier Guillaume <courrier.guillaume.123@gmail.com>
2020-07-12 09:20:07 +0200

    add powerline font

--- st/config.h
+++ st/config.h
@@ -5,8 +5,9 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
+/* static char *font = "monospace:pixelsize=18:antialias=true:autohint=true"; */
 static char *font = "monospace:pixelsize=18:antialias=true:autohint=true";
-static char *font2[] = { "JoyPixels:pixelsize=10:antialias=true:autohint=true" };
+static char *font2[] = { "JoyPixels:pixelsize=10:antialias=true:autohint=true", "Inconsolata for Powerline:pixelsize=19:antialias=true:autohint=true" };
 static int borderpx = 2;
 
 /*
