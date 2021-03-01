814741a411596c0312cd8785beb04c69b7a41d57
Courrier Guillaume <courrier.guillaume.123@gmail.com>
2021-03-01 10:42:48 +0100

    Add backlight information in the status bar and update it from dwm

--- dwmblocks/blocks.def.h
+++ dwmblocks/blocks.def.h
@@ -1,12 +1,13 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
-	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-	{"",      "volume",                       0,                  10},
-	{"",      "internet",                     5,                   4},
-	{"",      "memory",                      10,                  14},
-	{"",      "cpu",                         10,                  13},
-	{"",      "battery | tr \'\n\' \' \'",    5,                   3},
-	{"",      "clock",                        1,                   8},
+  /*Icon*/  /*Command*/    /*Update Interval*/    /*Update Signal*/
+  {"",      "brightness",                   0,                  11},
+  {"",      "volume",                       0,                  10},
+  {"",      "internet",                     5,                   4},
+  {"",      "memory",                      10,                  14},
+  {"",      "cpu",                         10,                  13},
+  {"",      "battery | tr \'\n\' \' \'",    5,                   3},
+  {"",      "clock",                        1,                   8},
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
