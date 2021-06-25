610a3cfab6c87447c34481175a21dd74aa4c3625
Courrier Guillaume <courrier.guillaume.123@gmail.com>
2021-06-25 22:13:19 +0200

    st: add font2 patch

--- st/config.h
+++ st/config.h
@@ -5,7 +5,11 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "Liberation Mono:pixelsize=12:antialias=true:autohint=true";
+static char *font = "Ubuntu Mono:pixelsize=18:antialias=true:autohint=true";
+static char *font2[] = {
+    "JoyPixels:pixelsize=10:antialias=true:autohint=true",
+    "Inconsolata for Powerline:pixelsize=17:antialias=true:autohint=true"
+};
 static int borderpx = 2;
 
 /*
