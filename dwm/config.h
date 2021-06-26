5c740ec27f79cc95912c0ea40c290e7b89f870ab
Courrier Guillaume <courrier.guillaume.123@gmail.com>
2021-06-26 23:01:48 +0200

    dwm: send signal to dwmblocks when changing backlight

--- dwm/config.h
+++ dwm/config.h
@@ -239,18 +239,17 @@ static Key keys[] = {
 	// decrease vertical gaps around clients
 	{ MODKEY|ShiftMask|ControlMask, XK_o,      incrovgaps,  {.i = -3 } },
 
-	{ 0,         XK_Print,                 spawn, SHCMD("flameshot full -p ~/Pictures/screenshots") },
-	{ ShiftMask, XK_Print,                 spawn, SHCMD("flameshot gui -p ~/Pictures/screenshots") },
+	{ 0,         XK_Print,                 spawn, SHCMD("flameshot full -p ~/Pictures/screenshots")                },
+	{ ShiftMask, XK_Print,                 spawn, SHCMD("flameshot gui -p ~/Pictures/screenshots")                 },
 	{ 0,         XF86XK_Sleep,             spawn, SHCMD("slock")                                                   },
 	{ 0,         XF86XK_AudioMute,         spawn, SHCMD("pamixer -t; kill -44 $(pidof dwmblocks)")                 },
 	{ 0,         XF86XK_AudioRaiseVolume,  spawn, SHCMD("pamixer --allow-boost -i 3; kill -44 $(pidof dwmblocks)") },
 	{ 0,         XF86XK_AudioLowerVolume,  spawn, SHCMD("pamixer --allow-boost -d 3; kill -44 $(pidof dwmblocks)") },
-	{ 0,         XF86XK_MonBrightnessUp,   spawn, SHCMD("xbacklight -inc 15")                                      },
-	{ 0,         XF86XK_MonBrightnessDown, spawn, SHCMD("xbacklight -dec 15")                                      },
+	{ 0,         XF86XK_MonBrightnessUp,   spawn, SHCMD("xbacklight -inc 15; kill -45 $(pidof dwmblocks)")         },
+	{ 0,         XF86XK_MonBrightnessDown, spawn, SHCMD("xbacklight -dec 15; kill -45 $(pidof dwmblocks)")         },
 	{ 0,         XF86XK_TouchpadToggle,    spawn, SHCMD("toggle-touchpad")                                         },
-	{ 0,         XF86XK_ScreenSaver,       spawn, SHCMD("[ $(xbacklight -get | sed \"s/\\.[0-9]\\+//\") = \"0\" ] && xbacklight -set 100 || xbacklight -set 0") },
-
-	{ MODKEY,           XK_F1,      spawn,          SHCMD("bootmenu") },
+	{ 0,         XF86XK_ScreenSaver,       spawn, SHCMD("toggle-backlight")                                        },
+	{ MODKEY,    XK_F1,                    spawn, SHCMD("bootmenu")                                                },
 };
 
 
